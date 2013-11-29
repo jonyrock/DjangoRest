3a7ed0946dc7685c0d1dd1518840c53d4b67f82c
jonyrock <ilms@live.ru>
2013-11-29 15:59:57 +0400

    test2

--- DjangoRest/urls.py
+++ DjangoRest/urls.py
@@ -1,14 +1,6 @@
-from django.conf.urls import patterns, url, include
-from rest_framework import routers
-from DjangoRestApp import views
+from django.conf.urls import patterns, url
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browseable API.
-urlpatterns = patterns('',
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+urlpatterns = patterns('DjangoRestApp.views',
+    url(r'^snippets/$', 'snippet_list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
 )
--- DjangoRestApp/views.py
+++ DjangoRestApp/views.py
@@ -1,17 +1,62 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from serializers import UserSerializer, GroupSerializer
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from models import Snippet
+from serializers import SnippetSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
+class JSONResponse(HttpResponse):
     """
-    API endpoint that allows users to be viewed or edited.
+    An HttpResponse that renders its content into JSON.
     """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    
-class GroupViewSet(viewsets.ModelViewSet):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+        
+@csrf_exempt
+def snippet_list(request):
     """
-    API endpoint that allows groups to be viewed or edited.
+    List all code snippets, or create a new snippet.
     """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        else:
+            return JSONResponse(serializer.errors, status=400)
+        
+        
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        else:
+            return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
